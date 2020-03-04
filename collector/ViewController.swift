e91b3426af9733f05c43396a4b90d487fa78f591
Brian Arend <brian.arend@gmail.com>
2020-03-04 08:50:26 -0700

    Initial Commit

---
+++ collector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collector
+//
+//  Created by Brian on 3/4/20.
+//  Copyright © 2020 Brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
