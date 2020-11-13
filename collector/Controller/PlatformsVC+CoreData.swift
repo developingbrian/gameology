6f645cf3de39084034ea13166994de6ae42ffd68
developingbrian <brian.arend@gmail.com>
2020-11-13 09:31:50 -0700

    platformsvc

---
+++ collector/Controller/PlatformsVC+CoreData.swift
@@ -0,0 +1,28 @@
+//
+//  PlatformsVC+CoreData.swift
+//  collector
+//
+//  Created by Brian on 10/21/20.
+//  Copyright © 2020 Brian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+
+extension PlatformsVC {
+    
+
+
+func fetchCoreDataPlatformObject(id: Int) -> Platform {
+    
+    let platform = persistenceManager.fetchFilteredByPlatform(Platform.self, platformID: id)
+    print(platform)
+    let platformobj = platform[0]
+    
+    return platformobj
+}
+
+
+}
