9d2e91bb09a2bb9be87801c543a03c5b89dcf913
developingbrian <brian.arend@gmail.com>
2022-02-11 22:21:12 -0700

    Untrack files in .gitignore

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.3'
+
+target 'collector' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for collector
+
+  pod 'SOTabBar'
+
+end
--- collector/Constants.swift
+++ collector/Constants.swift
@@ -9,25 +9,13 @@
 import Foundation
 
 struct Constants {
-static let igdbAPIKey = "886c478126bb68dd167a6eeec66f0922"
-static let screenScaperDevID = "thesuffering"
-static let screenScraperDevPassword = "06QOJ0pDreS"
-static let gameDBAPIKey = "8b574d53e9ff612a214486cfe8def1f5c045b0e4eaac50cfd54aa7873d89fd7b"
-static let gameDBAPIKeyip = "9ae02cc7fbae0bab0e316ad739c7c335b77062eb008a67f14990ecc8b14660e2"
-static let youtubeEmbedURL = "https://www.youtube.com/embed/"
-
+    static let youtubeEmbedURL = "https://www.youtube.com/embed/"
     
 }
 
 enum Constant {
-    static let igdbAPIKey = "886c478126bb68dd167a6eeec66f0922"
-    static let screenScaperDevID = "thesuffering"
-    static let screenScraperDevPassword = "06QOJ0pDreS"
-    static let gameDBAPIKeyip = "9ae02cc7fbae0bab0e316ad739c7c335b77062eb008a67f14990ecc8b14660e2"
-    static let gameDBAPIKey = "8b574d53e9ff612a214486cfe8def1f5c045b0e4eaac50cfd54aa7873d89fd7b"
     static let youtubeEmbedURL = "https://www.youtube.com/embed/"
-    static let twitchClientID = "8og8u13zzj49cru9v0qgp386m3g39e"
-    static let twtichClientSecret = "dtwp54ffq3cyocz5dygktc3mzby8dc"
+    
 }
 
 enum baseURL : String {
@@ -42,12 +30,12 @@ enum baseURL : String {
     case micro = "https://images.igdb.com/igdb/image/upload/t_micro/"
     case hd = "https://images.igdb.com/igdb/image/upload/t_720p/"
     case fullHD =
-    "https://images.igdb.com/igdb/image/upload/t_1080p/"
+            "https://images.igdb.com/igdb/image/upload/t_1080p/"
 }
 
 enum SortSection: String, CaseIterable {
-   case title = "title"
-   case platformName = "platformID"
-   case genre = "genre"
-   case releaseDate = "releaseYear"
+    case title = "title"
+    case platformName = "platformID"
+    case genre = "genre"
+    case releaseDate = "releaseYear"
 }
